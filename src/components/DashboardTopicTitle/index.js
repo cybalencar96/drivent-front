1a5735a013a90a95be378686b9299d47d0f35786
Carlos Alencar <iuryalencar@poli.ufrj.br>
2022-01-19 00:07:22 -0300

    checkpoint finish PaymentTopic

--- src/components/DashboardTopicTitle/index.js
+++ src/components/DashboardTopicTitle/index.js
@@ -2,7 +2,11 @@ import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
 
 const DashboardTopicTitle = styled(Typography)`
-  margin-bottom: 20px!important;
+  font-family: "Roboto", sans-serif;
+  color: #8e8e8e;
+  font-size: 20px;
+  line-height: 23px;
+  margin: 10px 0 !important;
 `;
 
 export { DashboardTopicTitle };
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -3,6 +3,11 @@ import { DashboardTitle } from "../../../components/DashboardTitle";
 import UserContext from "../../../contexts/UserContext";
 import { NoContent } from "../../../components/NoContent";
 import { PaymentCard } from "../../../components/PaymentCard";
+import { DashboardTopicTitle } from "../../../components/DashboardTopicTitle";
+
+const textTopic1 = "Primeiro, escolha sua modalidade de ingresso";
+const textTopic2 = "Ótimo! Agora escolha sua modalidade de hospedagem";
+const textTopicEnd = "Fechado! O total ficou em R$ 600. Agora é só confirmar:";
 
 export default function Payment() {
   const { userData } = useContext(UserContext);
@@ -11,8 +16,17 @@ export default function Payment() {
     <>
       <DashboardTitle variant="h4">Ingresso e pagamento</DashboardTitle>
       {
-        !userData.user.enrolled ? <NoContent text={text} /> : <PaymentCard title="Online" price={100}/>
+        !userData.user.enrolled ? <NoContent text={text} /> : <PaymentTopic text={textTopic1}/>
       }
+    </>
+  );
+}
+
+function PaymentTopic({ text }) {
+  return (
+    <>
+      <DashboardTopicTitle variant="h6">{text}</DashboardTopicTitle>
+      <PaymentCard title="Online" price={100}/>
     </>
   );
 }
