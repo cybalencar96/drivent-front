34146fbf1b64f8f09686cf0e85dbd7f55aa8d44a
Carlos Alencar <iuryalencar@poli.ufrj.br>
2022-01-18 23:24:42 -0300

    add no content message

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,9 +1,17 @@
+import { useContext } from "react";
 import { DashboardTitle } from "../../../components/DashboardTitle";
+import UserContext from "../../../contexts/UserContext";
+import { NoContent } from "../../../components/NoContent";
 
 export default function Payment() {
+  const { userData } = useContext(UserContext);
+  const text = "Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem";
   return (
     <>
       <DashboardTitle variant="h4">Ingresso e pagamento</DashboardTitle>
+      {
+        !userData.user.enrolled ? <NoContent text={text} /> : ""
+      }
     </>
   );
 }
