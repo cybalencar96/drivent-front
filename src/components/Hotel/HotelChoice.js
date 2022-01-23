11ae923393834d7565833c239354ee750319230f
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2022-01-22 21:35:03 -0300

    merge

--- src/components/Hotel/HotelChoice.js
+++ src/components/Hotel/HotelChoice.js
@@ -1,12 +1,61 @@
 import Container from "./container";
+import useApi from "../../hooks/useApi";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import HotelCard from "../HotelCard";
 
 export default function HotelChoice({ ticket }) {
+  const { hotel } = useApi();
+  const [hotelsData, setHotelsData] = useState([]);
+
+  useEffect(() => {
+    hotel
+      .getHotels()
+      .then((response) => setHotelsData(response.data))
+      .catch(() => alert("Erro"));
+  }, []);
+
   return ticket.hotelPrice === "0.00" || ticket.hotelPrice === null ? (
     <Container>
       <p>Sua modalidade de ingresso não inclui hospedagem</p>
       <p>Prossiga para a escolha de atividades</p>
     </Container>
   ) : (
-    "em breve"
+    <HotelSectionContainer>
+      <h1>Escolha de hotel e quarto:</h1>
+      <h2>Primeiro, escolha seu hotel</h2>
+
+      <div>
+        {hotelsData?.map((hotel) => (
+          <HotelCard
+            key={hotel.id}
+            hotelId={hotel.id}
+            imageUrl={hotel.imageUrl}
+            hotelTitle={hotel.name}
+          />
+        ))}
+      </div>
+    </HotelSectionContainer>
   );
 }
+
+const HotelSectionContainer = styled.section`
+  font-family: "Roboto", sans-serif;
+
+  > h1 {
+    margin-bottom: 40px;
+    font-size: 30px;
+  }
+
+  > h2 {
+    color: #8e8e8e;
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+  > div {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
+`;
