4bf3e7ad4814250aef35b24c47601fbd093f3e1e
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2022-01-29 17:50:37 -0300

    added payment and event type confirmations

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import styled from "styled-components";
 import LocationSection from "./LocationsSection";
 import Title from "./Title";
 import UserApi from "../../services/UserApi";
-import { useEffect } from "react";
 import DateButton from "./DateButton";
 import * as helper from "./helpers";
+import UserContext from "../../contexts/UserContext";
 
 export default function Activities() {
   const [activities, setActivities] = useState([]);
   const [filterCase, setFilterCase] = useState(null);
   const [clickedOnceAtLeast, setClickedOnceAtLeast] = useState(false);
+  const { userData } = useContext(UserContext);
 
   function filterSelectionHandler(activity) {
     setFilterCase(helper.formatDateToButtonPattern(activity));
@@ -17,23 +19,54 @@ export default function Activities() {
   }
 
   useEffect(() => {
-    UserApi
-      .getAllEvents()
-      .then(response => {
+    UserApi.getAllEvents()
+      .then((response) => {
         setActivities(response.data);
-        console.log(response.data);})
-      .catch(e => console.log(e));
+        console.log(response.data);
+      })
+      .catch((e) => console.log(e));
   }, []);
 
   return (
     <>
       <Title>Escolha de atividades</Title>
-      {
-        helper
-          .getUnique("startDate", activities)
-          .map(activity => <DateButton onClick={activity => filterSelectionHandler(activity)}>{helper.formatDateToButtonPattern(activity)}</DateButton>)
-      }
-      <LocationSection activities={activities}></LocationSection>
+      {userData.user.paid === undefined ? (
+        <Container>
+          <p>Você precisa ter confirmado pagamento antes</p>
+          <p>de fazer a escolha de atividades</p>
+        </Container>
+      ) : userData.user.paid.type.name === "Online" ? (
+        <Container>
+          <p>Sua modalidade de ingresso não necessita escolher</p>
+          <p>atividade. Você terá acesso a todas as atividades.</p>
+        </Container>
+      ) : (
+        <>
+          {helper.getUnique("startDate", activities).map((activity) => (
+            <DateButton
+              onClick={(activity) => filterSelectionHandler(activity)}
+            >
+              {helper.formatDateToButtonPattern(activity)}
+            </DateButton>
+          ))}
+          <LocationSection activities={activities}></LocationSection>
+        </>
+      )}
     </>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  max-height: 560px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    font-size: 20px;
+    line-height: 23px;
+    color: #8e8e8e;
+  }
+`;
